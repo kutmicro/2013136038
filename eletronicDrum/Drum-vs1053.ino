77aacd1748521bf878b4092e6646493bd18b4aaa
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-15 16:58:17 +0900

    Update Drum-vs1053.ino

--- eletronicDrum/Drum-vs1053.ino
+++ eletronicDrum/Drum-vs1053.ino
@@ -26,7 +26,7 @@
 // Don't forget to connect the GPIO #1 pin to 3.3V and the RX pin to digital #2
 
 // See http://www.vlsi.fi/fileadmin/datasheets/vs1053.pdf Pg 31
-#define VS1053_BANK_DEFAULT 0x00
+#define VS1053_BANK_DEFAULT 0x00  //디폴트 소리값
 #define VS1053_BANK_DRUMS1 0x78
 #define VS1053_BANK_DRUMS2 0x7F
 #define VS1053_BANK_MELODY 0x79
@@ -35,7 +35,7 @@
 #define VS1053_GM1_OCARINA 80
 
 #define MIDI_NOTE_ON  0x90
-#define MIDI_CHAN_MSG 0xB0
+#define MIDI_CHAN_MSG 0xB0 
 #define MIDI_CHAN_BANK 0x00
 #define MIDI_CHAN_VOLUME 0x07
 #define MIDI_CHAN_PROGRAM 0xC0
